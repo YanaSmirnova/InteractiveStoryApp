cd3a5231a65f63400242b7e3543b2e1fcfee6776
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-01 11:43:29 +1100

    Connected new StoryActivity to mainActivity

--- app/src/main/java/yanasmirnova/com/interactivestory/MainActivity.java
+++ app/src/main/java/yanasmirnova/com/interactivestory/MainActivity.java
@@ -1,11 +1,11 @@
 package yanasmirnova.com.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,9 +25,14 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory();
             }
         });
+    }
+
+    private void startStory() {
+        Intent intent = new Intent(this, StoryActivity.class);
+        startActivity(intent);
     }
 
 }
