8b22e110843f239e020b3b9af19d1c3682486567
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-03 12:27:17 +1100

    Set variables to represent views and added method to load page

--- app/src/main/java/yanasmirnova/com/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/yanasmirnova/com/interactivestory/ui/StoryActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import yanasmirnova.com.interactivestory.R;
+import yanasmirnova.com.interactivestory.model.Page;
+import yanasmirnova.com.interactivestory.model.Story;
 
 
 public class StoryActivity extends ActionBarActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,15 @@ public class StoryActivity extends ActionBarActivity {
         String name = intent.getStringExtra(getString(R.string.key_name));
 
         Log.d(TAG, name);
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceBtn1);
+        mChoice2 = (Button)findViewById(R.id.choiceBtn2);
+    }
+
+    private void loadPage() {
+        Page page = mStory.getPage(0);
     }
 
 }
